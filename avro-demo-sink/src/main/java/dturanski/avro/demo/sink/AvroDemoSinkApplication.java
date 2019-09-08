1661333afa611d8ae231750af480f1a7d80cfd6c
David Turanski <dturanski@pivotal.io>
2019-09-08 13:48:34 -0400

    Add sleuth and README

--- avro-demo-sink/src/main/java/dturanski/avro/demo/sink/AvroDemoSinkApplication.java
+++ avro-demo-sink/src/main/java/dturanski/avro/demo/sink/AvroDemoSinkApplication.java
@@ -2,14 +2,17 @@ package dturanski.avro.demo.sink;
 
 import dturanski.avro.demo.domain.User;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
+@Slf4j
 public class AvroDemoSinkApplication {
 
     public static void main(String[] args) {
@@ -17,7 +20,8 @@ public class AvroDemoSinkApplication {
     }
 
     @StreamListener(Sink.INPUT)
-    public void process(User user) {
-        System.out.println(user);
+    public void process(Message<User> message) {
+
+       log.info("received: {}", message);
     }
 }
